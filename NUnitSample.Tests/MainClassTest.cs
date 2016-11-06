6ab0efa203a599ee60e10649255f989a36a0fb94
budougumi0617 <budougumi0617@gmail.com>
2016-11-06 19:25:22 +0900

    Add AppVeyor setting

--- NUnitSample.Tests/MainClassTest.cs
+++ NUnitSample.Tests/MainClassTest.cs
@@ -1,36 +1,33 @@
 ﻿using System;
 using CSharpTraining;
 using MonoBrickFirmware.Movement;
-using NUnit.Framework;
 
-namespace NUnitSample.Tests
+namespace XUnitSample.Tests
 {
 	/// <summary>
 	/// Sample test class
 	/// </summary>
-	/// <remarks>You can use NUnit console too. There is in below path.
-	/// .\packages\NUnit.Runners.2.6.4\tools\nunit.exe
+	/// <remarks>
+	/// It was described below URL how to use XUnit
+	/// https://xunit.github.io/docs/comparisons.html
 	/// </remarks>
-	[TestFixture]
 	public class MainClassTests
 	{
-		[Test, Description("We can describe test summary by this attribute."), Category("We can set test category by this attribute.")]
+		[Xunit.Fact(DisplayName = "We can describe test summary by this attribute."), Xunit.Trait("Category", "Sample")]
 		public void MainTest()
 		{
-			Assert.IsTrue(true);
-			Assert.AreEqual(1, 1);
+			Xunit.Assert.True(true);
+			Xunit.Assert.Equal(1, 1);
 			var foo = new Object();
 			var same = foo;
-			Assert.AreSame(foo, same); // Verify their variables are same object.
+			Xunit.Assert.Same(foo, same); // Verify their variables are same object.
 		}
 
 
-		[Test, Description("If We confirm that the targert method throws an exeception, we use ExpectedException attribute.")]
-		[ExpectedException(typeof(InvalidOperationException))]
-		[Explicit("If you want to ignore test case, you set this attribute to the test case.")]
+		[Xunit.Fact(Skip="If you want to ignore test case, you set this attribute to the test case.")]
 		public void IgnoreTest()
 		{
-			Assert.Fail("Expected this test case does not be executed.");
+			Xunit.Assert.True(false, "Expected this test case does not be executed.");
 			MainClass.Main(new string[] { "" });
 			Motor dummy = new Motor(MotorPort.OutA);
 			dummy.GetSpeed();
