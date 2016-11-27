83ee920ae5f77f8e1f7d452badb07c7344eb2e8d
budougumi0617 <budougumi0617@gmail.com>
2016-11-27 19:04:09 +0900

    Fix up using

--- SandBox/Program.cs
+++ SandBox/Program.cs
@@ -23,12 +23,8 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-using System;
-using System.ComponentModel.Composition.Hosting;
-using Microsoft.Practices.ServiceLocation;
-using System.Reflection;
 using System.ComponentModel.Composition;
-using Plugins.Core;
+using System.ComponentModel.Composition.Hosting;
 
 namespace SandBox
 {
