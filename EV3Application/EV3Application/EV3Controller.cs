e0d213ee30f1b3a2d5ee7898ec2235e121b14b08
YN6127yn <yohjiroh.nanameki@jrits.ricoh.co.jp>
2016-12-07 11:56:23 +0900

    仮実装のソースコードです

---
+++ EV3Application/EV3Application/EV3Controller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MonoBrickFirmware.UserInput;
+
+namespace EV3Application
+{
+	/// <summary>
+	/// EV3デバイスの全体を制御するクラス。
+    /// 各コントローラーに処理を依頼する。
+	/// </summary>
+	public class EV3Controller
+	{
+		/// <summary>
+		/// デフォルトコンストラクタ。
+		/// </summary>
+		public EV3Controller ()
+		{
+		}
+
+		/// <summary>
+		/// <see cref \= "EV3Applicationl.LCDController"/>にLCD画面の処理を指示する。
+        /// EnterButtonが押下された際に<see cref \= "EV3Applicationl.LCDController"/>に知らせる。
+		/// </summary>
+		public void ControlEV3(){
+
+			ManualResetEvent sendSignal = new ManualResetEvent (false);
+			LCD.LCDController controller = new LCD.LCDController (sendSignal);
+			ButtonEvents bevent = new ButtonEvents ();
+			bevent.EnterPressed += () => {
+				controller.EneterPressed();
+			};
+			controller.ControlLCD ();
+		}
+	}
+}
---
+++ EV3Application/EV3Application/IDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EV3Application.LCD
+{
+    /// <summary>
+    /// インターフェイス
+    /// LCD画面に表示する文字列またはダイアログの内容と、表示するメソッドをメンバとして持つ。
+    /// </summary>
+	public interface IDisplay
+	{
+		/// <summary>
+		/// 表示する文字列、またはダイアログの内容。
+		/// </summary>
+        string Message{ get; set;}
+
+        /// <summary>
+        /// LCD画面上に文字列、またはダイアログを表示する。
+        /// </summary>
+		void Show();
+	}
+}
