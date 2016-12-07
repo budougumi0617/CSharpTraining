84b5849c0c840d0774804e86915d73324846c7a9
YN6127yn <yohjiroh.nanameki@jrits.ricoh.co.jp>
2016-12-07 12:01:19 +0900

    間違って操作してしまったので、commitし直します 仮実装済みのファイルです 実装はありますが、クラスと関数のヘッダ部分について確認お願いします

---
+++ EV3Application/EV3Application/EV3Start.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace EV3Application
+{
+	/// <summary>
+	/// EV3アプリケーションを開始と終了を管理する。
+	/// </summary>
+	class EV3Start
+	{
+		/// <summary>
+		/// アプリケーションの開始点であり、開始と終了を管理する。
+		/// </summary>
+		public static void Main ()
+		{
+			EV3Controller controller = new EV3Controller ();
+			controller.ControlEV3 ();
+		}
+	}
+}
+
