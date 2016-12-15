3555577ed4a7b98f6db5f01dbd0686faa4ea8a4f
YN6127yn <yohjiroh.nanameki@jrits.ricoh.co.jp>
2016-12-15 13:43:08 +0900

    Test updated

--- EV3Application.Test/EV3ControllerTest.cs
+++ EV3Application.Test/EV3ControllerTest.cs
@@ -1,19 +1,24 @@
 ﻿using System;
 using NUnit.Framework;
-using MonoBrickFirmware;
-using MonoBrickFirmwareWrapper;
 using EV3Application;
-using System.Reflection;
-using System.Threading;
 
 namespace EV3Application.Test
 {
 	[TestFixture]
 	public class EV3ControllerTest
 	{
-		public EV3ControllerTest ()
+		//ControlEV3のテスト
+		//各コントローラーを使用して、機能を実現する。
+
+		[Test, Description("ControlLCDを呼び出し、機能が実現できているか")]
+		public void ControlEV3Test001 ()
 		{
+			//準備
+			EV3Controller controller = new EV3Controller();
+			//実施、確認
+			Assert.DoesNotThrow(
+				() => controller.ControlEV3()
+			);
 		}
 	}
 }
-
---
+++ EV3Application.Test/EV3StartTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using EV3Application;
+
+namespace EV3Application.Test
+{
+	[TestFixture]
+	public class EV3StartTest
+	{
+		
+		[Test, Description("ControlEV3が呼び出し、アプリケーションが開始できているか")]
+		public void MainTest001 ()
+		{
+			//実施、確認
+			Assert.DoesNotThrow (
+				() => EV3Start.Main ()
+			);
+		}
+	}
+}
+
