85e3afcefa9c2751213f3ece59ad52fe55cb15d7
YN6127yn <yohjiroh.nanameki@jrits.ricoh.co.jp>
2016-12-28 17:43:15 +0900

    add assembly information

--- EV3Application/AssemblyInfo.cs
+++ EV3Application/AssemblyInfo.cs
@@ -11,4 +11,7 @@ using System.Runtime.CompilerServices;
 
 [assembly:AssemblyTitle("EV3デバイスのLCDに文字列やダイアログを表示する")]
 [assembly:InternalsVisibleTo("EV3Application.Test")]
-
+[assembly:AssemblyProduct("EV3Application")]
+[assembly:AssemblyCopyright("Yojiro Nanameki")]
+[assembly:AssemblyVersion("1.0.0.0")]
+[assembly:AssemblyFileVersion("1.0.0.0")]
